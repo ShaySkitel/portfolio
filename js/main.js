b1772677c716b914141cc26257bc72e85431ea64
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2022-12-05 15:21:47 +0200

    Add dynamic rendering functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log('Starting up');
+'use strict'
+
+$(document).ready(onInit)
+
+function onInit() {
+    console.log('Starting up');
+    renderProjects()
+}
+
+function renderProjects() {
+    const projects = getProjects()
+    const $elProjContainer = $('.projects-container')
+
+    $elProjContainer[0].innerHTML = projects.map(project => {
+        return `
+        <div class="col-md-4 col-sm-6 portfolio-item">
+          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fa fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src="img/portfolio/${project.id}.jpg" alt="${project.name}">
+          </a>
+        <div class="portfolio-caption">
+          <h4>${project.name}</h4>
+          <p class="text-muted">${project.title}</p>
+        </div>
+      </div>
+      `
+    }).join('')
+}
---
+++ js/services/proj-service.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const gProjects = [
+    {
+        id: "minesweeper",
+        name: "Minesweeper",
+        title: "Don't click the mines!",
+        desc: "placeholder",
+        url: "https://shayskitel.github.io/proj-minesweeper/",
+        publishedAt: 1669315611,
+        labels: ["Matrixes", "Mouse events"],
+    }
+]
+
+function getProjects(){
+    return gProjects
+}
